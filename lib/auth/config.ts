e45fab7088e76d814649d7fd64e17af6245586f6
Tekmindlabs <150348873+Tekmindlabs@users.noreply.github.com>
2024-12-28 12:58:20 +0500

    update

--- lib/auth/config.ts
+++ lib/auth/config.ts
@@ -3,6 +3,7 @@ import GoogleProvider from "next-auth/providers/google";
 import EmailProvider from "next-auth/providers/email";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
+import { authConfig as authOptions } from "@/lib/auth/config";
 
 export const authConfig: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
